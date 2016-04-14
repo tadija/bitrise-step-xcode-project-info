0877e29e255b1ea6f640daba39838eb264a1bc7f
Marko Tadic <tadija@me.com>
2016-04-14 17:44:09 +0200

    Created initial version of step bash script

--- step.sh
+++ step.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+# ---------------------
+# --- Exit if command fails
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-#  envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+set -e
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+# ---------------------
+# --- Required Input
+
+if [ -z "${info_plist_path}" ]; then
+	echo "[ERROR] Required input is missing: please provide path to Info.plist file"
+	exit 1
+fi
+
+# ---------------------
+# --- Main
+
+DIRECTORY=$(dirname "${info_plist_path}")
+FILENAME=$(basename "${info_plist_path}")
+
+echo "|"
+echo "[INFO] Switching working dir to [${DIRECTORY}]"
+cd $DIRECTORY
+
+if [ ! -f "${FILENAME}" ]; then
+	echo "[ERROR] File not found at given path: [${info_plist_path}]"
+	exit 1
+fi
+
+echo "[INFO] Reading data from [${info_plist_path}]..."
+
+version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${FILENAME}")
+build=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${FILENAME}")
+
+envman add --key APP_VERSION --value "${version}"
+echo "[INFO] Version: ${version} -> Saved to \$APP_VERSION environment variable."
+
+envman add --key APP_BUILD --value "${build}"
+echo "[INFO] Build: ${build} -> Saved to \$APP_BUILD environment variable."
+
+echo "[INFO] Finished extracting data from [${info_plist_path}]."
