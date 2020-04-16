bb6bcbc202c4220fee2006487ba53fdd3a793887
dlajarretie <dlajarretie@airintservices.com>
2020-04-16 18:40:46 +0200

    Add support for Xcode 11+

--- step.sh
+++ step.sh
@@ -16,6 +16,8 @@ fi
 # ---------------------
 # --- Main
 
+ORIGIN_PATH=$(pwd)
+
 DIRECTORY=$(dirname "${info_plist_path}")
 FILENAME=$(basename "${info_plist_path}")
 
@@ -32,6 +34,29 @@ echo "[INFO] Reading data from [${info_plist_path}]..."
 
 version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${FILENAME}")
 build=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${FILENAME}")
+
+# Xcode 11 projects specific behaviour
+if [ $version == '$(MARKETING_VERSION)' ] || [ $build == '$(CURRENT_PROJECT_VERSION)' ]; then
+	echo "[INFO] Xcode 11+ project detected, reading version & build number from xcodeproj instead..."
+	cd "${ORIGIN_PATH}"
+	if [ -z "${xcodeproj_path}" ]; then
+		echo "[ERROR] Please provide correct path to the xcodeproj folder"
+		exit 1
+	fi
+	if [ -z "${target}" ]; then
+		TARGET_SPECIFIER=""
+	else
+		TARGET_SPECIFIER="-target '${target}'"
+	fi
+	echo "[INFO] Reading data from [${xcodeproj_path}]..."
+	PROJECT_SETTINGS=$(/bin/sh -c "xcodebuild -project ${xcodeproj_path} $TARGET_SPECIFIER -showBuildSettings")
+	if [ $version == '$(MARKETING_VERSION)' ]; then
+		version=$( echo "$PROJECT_SETTINGS" | grep "MARKETING_VERSION" | sed 's/[ ]*MARKETING_VERSION = //')
+	fi
+	if [ $build == '$(CURRENT_PROJECT_VERSION)' ]; then
+		build=$( echo "$PROJECT_SETTINGS" | grep "CURRENT_PROJECT_VERSION" | sed 's/[ ]*CURRENT_PROJECT_VERSION = //')
+	fi
+fi
 
 envman add --key XPI_VERSION --value "${version}"
 echo "[INFO] Version: ${version} -> Saved to \$XPI_VERSION environment variable."
